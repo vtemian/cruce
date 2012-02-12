7a64cb8ba35476e8eb95e99a1ddb180a51694c37
unknown <prospero@Prospero-Unu.(none)>
2012-02-12 18:46:14 +0200

    2nd

--- client.js
+++ client.js
@@ -11,18 +11,26 @@ $(document).ready(function(){
 	var turaCarti = false;
 	var nrTure = 0;
 	var cerere;
+	var jocCurent;
 	
 	$("#submit").click(function(){
 		username = $('#user').val();
 		socket = io.connect('http://localhost:5555');
 		socket.on('identified', function(){
-			socket.emit('handshaking', { user: username });
+			//socket.emit('handshaking', { user: username });
+			console.log('trimit join_new_game');
+			socket.emit('join_new_game', { user: username });
 		})
 		$('#connection').remove();
+		$('.tromf').live('click', function(){
+			var value = $(this).data('value');
+			socket.emit('tromf', {'battleID':battleID, 'tromf':value, 'username':username});
+			$('#tromfii').toggleClass('visible');
+			turaCarti = true;
+		});
 		socket.on('online', function(data){
 			$('#online').html(data.online);
 		})
-	
 		socket.on('battle', function(data){
 			$('#status').html('You are playing with '+ data.enemy);
 			turn = data.free;
@@ -38,10 +46,44 @@ $(document).ready(function(){
 			});
 			
 		})
+
+		socket.on('startbattle', function(data){
+			var enemy = '';
+			jocCurent = data.joc;
+			for(p in data.joc.players){
+				enemy += data.joc.players[p].userdata.user + '; ';
+			}
+			$('#status').html('You are playing with '+ enemy);
+			turn = username == data.joc.players[0].userdata.user;
+			$('#licitatii').toggleClass('visible');
+			deck = data.joc.players[data.joc.currentplayerindex].hand;
+			$('#carti').toggleClass('visible');
+			
+			$.each($('.carte'), function(index, value){
+				$(value).html(deck[index].cardValue + deck[index].cardColor);
+				$(value).data('cardValue', deck[index].cardValue);
+				$(value).data('cardColor', deck[index].cardColor);
+			});
+			
+		})
+		
 		socket.on('licitatii', function(data){
+		if(data.finish == true)
+		{
 			$('#nrLicitatii').empty();
-			$('#nrLicitatii').append(data.licitatie.cate);
-			$('#nrLicitatii').append(data.licitatie.user);
+			$('#nrLicitatii').append("Licitatia s-a terminat castigarul este:");
+			$('#nrLicitatii').append(data.player.licitare);
+			$('#nrLicitatii').append(data.player.username);
+			return;
+		
+		}
+		
+		
+		alert(data.player.licitare + " - "+ data.player.username);
+			//$('#nrLicitatii').empty();
+			
+			$('#nrLicitatii').append(data.player.licitare);
+			$('#nrLicitatii').append(data.player.username);
 			battleID = data.battleID;
 			licitatii ++;
 			if(licitat && licitatii == 2){
@@ -50,6 +92,30 @@ $(document).ready(function(){
 			else
 				turn = true;
 		});
+		
+		
+		socket.on('licitatii_vechi_modificat', function(data){
+		if(data.finish == true)
+		{
+			$('#nrLicitatii').empty();
+			$('#nrLicitatii').append("Licitatia s-a terminat castigarul este:");
+			$('#nrLicitatii').append(data.player.licitare);
+			$('#nrLicitatii').append(data.player.username);
+			
+			socket.emit('licitatiiOver', {'battleID': data.battleID})			
+			turn = true;
+
+			
+			return;
+		}
+		
+			//$('#nrLicitatii').empty();
+			$('#nrLicitatii').append(data.player.licitare);
+			$('#nrLicitatii').append(data.player.username);
+			
+		});
+		
+		
 		socket.on('sealegetromf', function(){
 			alert('Se alege tromful');
 			$('#licitatii').toggleClass('visible');
@@ -57,12 +123,6 @@ $(document).ready(function(){
 		socket.on('alegetromf', function(){
 			$('#licitatii').toggleClass('visible');
 			$('#tromfii').toggleClass('visible');
-		});
-		$('.tromf').live('click', function(){
-			var value = $(this).data('value');
-			socket.emit('tromf', {'battleID':battleID, 'tromf':value, 'username':username});
-			$('#tromfii').toggleClass('visible');
-			turaCarti = true;
 		});
 		socket.on('tromfAles', function(data){
 			tromf = data.tromf;
@@ -72,23 +132,39 @@ $(document).ready(function(){
 			$('#cerere').html(cerere.cardValue + cerere.cardColor);
 			turaCarti = true;
 		});
+		socket.on('tura_incheiata', function(data){
+			$('#cerere').html('');
+			console.log('castigator = ' + data);
+			if(data.castigator == username){
+				turaCarti = true;
+			}else{
+				turaCarti = false;
+			}
+		});
+
+		socket.on('newplayeronline', function(data){
+			$('#newplayeronline').html(data.id + ':' + data.player.userdata.user);
+		})
+
+		
 	});
 	
 	$('.cate').live('click', function(){
 		var value = $(this).data('value');
-		if(turn && !licitat)
-			{socket.emit('licitatii', {'user': username, 'cate': value});licitat = true;}
-		else
-			alert('Nu-i tura ta!');
+		console.log(jocCurent);
+		if(!licitat)
+		{
+			socket.emit('licitatii', {'jocId':jocCurent.jocId,'user': username, 'cate': value});
+			licitat = true;
+		}else
+			alert('Nu-i tura ta sa licitezi! Ai licitat deja asteapta');
 	});
-	
-	
 	
 	$('.carte').live('click', function(){
 	
 		if(turaCarti){
 			if(cerere){
-				if(checCerere($(this))){
+				if(checkCerere($(this))){
 					cerere = false;
 					socket.emit('oferta', {'battleID': battleID, 'user':username, 'cardValue':$(this).data('cardValue'), 'cardColor':$(this).data('cardColor')});
 				}else{
@@ -104,7 +180,12 @@ $(document).ready(function(){
 				
 			}
 			turaCarti = false;
-			
+			//console.log('am emis oferta:');
+			var index = $('.carte').index($(this));
+			deck.splice(index, 1);
+			$(this).remove();
+		}else{
+			alert('nu este randul tau!');
 		}
 	});
 	
@@ -113,7 +194,7 @@ $(document).ready(function(){
 		var cardValue = card.data('cardValue');
 		var cardColor = card.data('cardColor');
 		
-		return false;
+		return true;
 	}	
 	
 })
--- server.js
+++ server.js
@@ -3,13 +3,66 @@ var users = new Array();
 var clients = new Array();
 var game = new Array();
 var onlineBattles = new Array();
+var onlineBattlesMircea = new Array();
 var licitatii = new Array();
 var score = new Array();
+var nr_jucatori = 2;
+var nrJocuri = 0;
 
 io.sockets.on('connection', function (socket) {
 	socket.emit('identified', { message: 'handshaking' });
 	
+	function getJocActiv(){
+		for(var i=0;i<onlineBattlesMircea.length; i++){
+			if(onlineBattlesMircea[i].players.length<nr_jucatori){
+				return onlineBattlesMircea[i];
+			}
+		}
+		var jocNou = 
+			{
+			  'jocId': nrJocuri//++
+			 ,'players': new Array()
+			 ,'deck':  getAllCards()
+			 ,'sockets': new Array()
+			};
+		onlineBattlesMircea.push(jocNou);
+		return jocNou;
+	}
+
+    socket.on('join_new_game', function (data) {
+		console.log('join_new_game: am primit:' + data);
+		var joc = getJocActiv();
+		joc.sockets.push(socket);
+		var player = {
+			'id':joc.players.length
+			, 'userdata':data
+			, 'hand':setDeckMircea(joc.deck)
+			, 'licitare':-1
+			};
+		joc.players.push(player);
+		//anunta adversarii ca a intrat un nou jucator
+		for(var i=0; i<joc.players.length; i++){
+			if(joc.players[i].userdata.user != player.userdata.user){
+				console.log('player=' + joc.players[i]);
+				var aSocket = joc.sockets[i];
+				aSocket.emit('newplayeronline', {'id':i,'player': player});
+			}
+		}
+		console.log('join_new_game: dupa  push');
+		if(joc.players.length == nr_jucatori){
+			for(p in joc.players){
+				console.log(joc.players[p]);
+				joc.sockets[p].emit('startbattle', {'joc': 
+					{
+						'jocId': joc.id
+						,'players':joc.players
+						, 'currentplayerindex':p}});
+			}
+		}
+	});	
+	
     socket.on('handshaking', function (data) {
+		console.log('id-ul este: ' + socket.id);
         users.push(data.user);
         clients.push(socket);
         
@@ -17,26 +70,96 @@ io.sockets.on('connection', function (socket) {
             clients[i].emit('online', {'online': clients.length});
         }
         var user = users.indexOf(data.user);
-	if(game.length>0 && (game.indexOf(clients[user]) == -1)){
-		decks = create_game();
-		console.log(decks);
-		game[0].emit('battle', {'enemy': data.user, 'free': true, 'deck': decks.firstDeck})
-		var user1 = clients.indexOf(game[0]);
-		socket.emit('battle', {'enemy': users[user1], 'free': false, 'deck': decks.secondDeck});
-		game.splice(0, 1);
-		
-		onlineBattles.push([data.user, users[user1]]);
-		
-		licitatii.push({'cate':0});
-		score.push(0);
-		
-	}else{
-		if(!game.length)
-		game.push(clients[user]);
+		if(game.length>0 && (game.indexOf(clients[user]) == -1)){
+			decks = create_game();
+			console.log(decks);
+			game[0].emit('battle', {'enemy': data.user, 'free': true, 'deck': decks.firstDeck})
+			var user1 = clients.indexOf(game[0]);
+			socket.emit('battle', {'enemy': users[user1], 'free': false, 'deck': decks.secondDeck});
+			game.splice(0, 1);
+			
+			onlineBattles.push([data.user, users[user1]]);
+			
+			licitatii.push({'cate':0});
+			score.push(0);
+		}else{
+			if(!game.length)
+				game.push(clients[user]);
 	}
     });
-    
+
+	function getJoc(indexJoc){
+	
+		if(indexJoc<=onlineBattlesMircea.length){
+			return onlineBattlesMircea[indexJoc];
+		}
+		return null;
+	}
+	
+	function findPlayer(joc, name){
+		for(p in joc.players){
+			if(joc.players[p].userdata.username == name){
+				return joc.players[p];
+			}
+		}
+		return null;
+	}
+	
+	function poateSaLiciteze(joc, player){
+		for(p in joc.players){
+	
+			if(joc.players[p].id == player.id){
+			
+				if(joc.players[p].licitare==-1){
+					return true;
+				}
+			}
+			
+			if(player.licitare == -1)
+			 {
+			 return false;
+			 }
+		}
+	}
+	
     socket.on('licitatii', function(data){
+    	var user = data.user;
+    	var licitat = data.cate;
+		console.log("licitatii jocId "+data)
+		var jocCurent = getJoc(data.jocId);
+		console.log("licitatii joc "+jocCurent)
+		if(jocCurent){
+			var player = findPlayer(jocCurent, data.user);
+			console.log("licitatii player "+player)
+			if(player && poateSaLiciteze(jocCurent, player)){
+			player.licitare = licitat;
+			
+			if(player.id ==nr_jucatori )
+			{
+			
+				//am ajuns sa liciteze ultimul player => s-a incheiat licitatia
+				//gasim cine a licitat cel mai mult si notificam corespunzator
+				
+				var max = -1;
+				var winner = -1;
+				for(p in jocCurent.players){
+					if(max< jocCurent.players.licitare)
+					{
+					   max = jocCurent.players.licitare;
+					   winner = p;
+					}
+				}
+				//am aflat castigatorul si licitarea maxima
+				for(p in jocCurent.players){					
+						jocCurent.sockets[p].emit('licitatii', {player: player, jocId: jocCurent.jocId, finish:true});
+				}
+				
+			}
+			}
+		}
+	});
+
+	socket.on('licitatii_vechi', function(data){
     	var user = data.user;
     	
     	var licitat = data.cate;
@@ -81,35 +204,51 @@ io.sockets.on('connection', function (socket) {
 		}
     	}
     })
+	
     socket.on('oferta', function(data){
-    
-    	var tromf = licitatii[data.battleID]['tromf'];
-    	var cardValue = data.cardValue;
-    	var cardColor = data.cardColor;
-    	//daca exista deja o cerere
-    	if(licitatii[data.battleID]['cerere'] != undefined ){
-    		check_carte(licitatii[data.battleID]['cerere'], cardValue, cardColor, tromf);
-    		licitatii[data.battleID]['cerere'] = undefined;
-    	}else{
-    		
-    		var cerere = {
-    			'cardValue': cardValue,
-    			'cardColor': cardColor
-    		}
-    		licitatii[data.battleID]['cerere'] = cerere;
-    		
-    		var userIndex = onlineBattles[data.battleID].indexOf(data.user);
-    		
-    		if(userIndex){
+		onlineBattles[data.battleId]
+		var tromf = licitatii[data.battleID]['tromf'];
+		var cardValue = data.cardValue;
+		var cardColor = data.cardColor;
+		//daca exista deja o cerere
+		if(licitatii[data.battleID]['cerere'] != undefined ){
+			var castigator = check_carte(licitatii[data.battleID]['cerere'], cardValue, cardColor, tromf);
+			for(index in onlineBattles){
+				console.log('index din online battles = ' + index);
+				console.log('online battles = ' + onlineBattles[index]);
+				//trimitem mesaj de castig la toti jucatorii
+				for(clienti in onlineBattles[index]){
+					var id = onlineBattles[index][clienti];
+					var c_obj_win = clients[users.indexOf(id)];
+					c_obj_win.emit('tura_incheiata', {'castigator':castigator, 'trimis_la':id});
+					if(1==2){
+						c_obj_win.emit('joc_incheiat', {'castigator': calculeaza_castigator(data.battleID) });
+					}
+				}
+			}
+
+
+
+			
+			licitatii[data.battleID]['cerere'] = undefined;
+		}else{
+			
+			var cerere = {
+				'cardValue': cardValue,
+				'cardColor': cardColor
+			}
+			licitatii[data.battleID]['cerere'] = cerere;
+			
+			var userIndex = onlineBattles[data.battleID].indexOf(data.user);
+			
+			if(userIndex){
 			clients[users.indexOf(onlineBattles[data.battleID][0])].emit('cerere', {'cerere': cerere});
 		}else{
 			clients[users.indexOf(onlineBattles[data.battleID][1])].emit('cerere', {'cerere': cerere});
 		}
-    	}
+		}
     })
-    function check_carte(cerere, ofertaValue, ofertaColor, tromf){
-    
-    }
+
     socket.on('disconnect', function (data){
         var index = clients.indexOf(socket);
 
@@ -130,6 +269,14 @@ io.sockets.on('connection', function (socket) {
             clients[i].emit('on', {'online': clients.length});
         }
     });
+
+	function calculeaza_castigator(battleID){
+	}
+	
+    function check_carte(cerere, ofertaValue, ofertaColor, tromf){
+		return users[0];
+    }
+
     function gameDroping(dropedUser){
         for(var index in onlineBattles){
             var userIndex = onlineBattles[index].indexOf(dropedUser);
@@ -142,7 +289,9 @@ io.sockets.on('connection', function (socket) {
         }
         return false;
     }
-    function create_game(){
+
+	
+	function getAllCards(){
     	var deck = new Array(
     		{
     			'cardValue': 2,
@@ -241,33 +390,33 @@ io.sockets.on('connection', function (socket) {
     			'cardColor': 'm'
     		}
     	);
-	
-	var numbers = new Array();
-	result = setDeck(deck, numbers);
-	var firstDeck = result.myDeck;
-	numbers = result.numbers;
-	result = setDeck(deck, numbers);
-	var secondDeck = result.myDeck;
-	
-	return {
-		'firstDeck': firstDeck,
-		'secondDeck': secondDeck
+		return deck;
 	}
-	    	
+
+	
+    function create_game(){
+		deck = getAllCards();
+		var numbers = new Array();
+		result = setDeck(deck, numbers);
+		var firstDeck = result.myDeck;
+		numbers = result.numbers;
+		result = setDeck(deck, numbers);
+		var secondDeck = result.myDeck;
+		
+		return {
+			'firstDeck': firstDeck,
+			'secondDeck': secondDeck
+		}
     }
-    function setDeck(deck, numbers){
+
+    function setDeckMircea(deck){
     	var myDeck = new Array();
     	for(var i=1; i<=6; i++){
-		var randomnumber = Math.floor(Math.random()*25)
-		index = numbers.indexOf(randomnumber);
-		while(index != -1){
-			var randomnumber = Math.floor(Math.random()*25)
-			index = numbers.indexOf(randomnumber);
+			var randomnumber = Math.floor(Math.random()*24)
+			myDeck.push(deck[randomnumber]);
+			deck.slice(randomnumber, 1);
 		}
-		numbers.push(randomnumber);
-		myDeck.push(deck[randomnumber-1]);
+		return myDeck;
 	}
-	
-	return {'myDeck': myDeck, 'numbers': numbers}
-    }
+
 });
